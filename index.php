bdb6211f2f21555da25a502bc0e74907e57c5625
Franck <50842288+Enhax@users.noreply.github.com>
2023-12-13 14:23:02 +0100

    new

--- index.php
+++ index.php
@@ -9,61 +9,81 @@ require_once 'views/parts/header.php';
 <div class="container">
     <!-- CATEGORIE ACTU -->
     <div class="sidebar-left">
-   <h2>ACTUALITÉS</h2>
-        
-<?php
-$url = "https://www.01net.com/actualites/feed/";
-$rss = simplexml_load_file($url);
+        <h2>ACTUALITÉS</h2>
 
-for ($i = 0; $i < 12; $i++) {
-    $item = $rss->channel->item[$i];
-    $datetime = date_create($item->pubDate);
-    $date = date_format($datetime, 'd M Y, H\hi');
-    $desc = $item->description;
-    $img = $item->enclosure['url'];
-?>
+        <?php
+        $url = "https://www.01net.com/actualites/feed/";
+        $rss = simplexml_load_file($url);
 
-    <ul>
-        <li>
-            <img src="<?php echo $img ?>">
-            <a href="<?php echo $item->link ?>"><?php echo $item->title ?></a>
-            <p><?php echo $desc ?></p>
-            <p><?php echo $date ?></p>
-        </li>
-    </ul>
-
-<?php
-}
-?>
+        for ($i = 0; $i < 12; $i++) {
+            $item = $rss->channel->item[$i];
+            $datetime = date_create($item->pubDate);
+            $date = date_format($datetime, 'd M Y, H\hi');
+            $desc = $item->description;
+            $img = $item->enclosure['url'];
+        ?>
+            <ul>
+                <li>
+                <img src="<?php echo $img ?>">
+                    <a href="<?php echo $item->link ?>"><?php echo $item->title ?></a>
+                    <p><?php echo $desc ?></p>
+                    <p><?php echo $date ?></p>
+                </li>
+            </ul>
+        <?php
+        }
+        ?>
 
     </div>
     <div class="sidebar-center">
-
         <?php
-        $rss = simplexml_load_file("https://www.jeuxactu.com/rss/mobile.rss");
+        $url = "https://www.jeuxactu.com/rss/mobile.rss";
+        $rss = simplexml_load_file($url);
 
-        echo '<h4>' . $rss->channel->title . '</h4>';
-
-        foreach ($rss->channel->item as $item) {
-            echo '<h4><a href="' . $item->link . '">' . $item->title . "</a></h4>";
-            echo "<p>" . $item->title . "</p>";
-            echo "<p>" . $item->description . "</p>";
+        for ($i = 0; $i < 12; $i++) {
+            $item = $rss->channel->item[$i];
+            $datetime = date_create($item->pubDate);
+            $date = date_format($datetime, 'd M Y, H\hi');
+            $desc = $item->description;
+            $img = $item->enclosure['url'];
+        ?>
+            <ul>
+                <li>
+                <img src="<?php echo $img ?>"><br>
+                    <a href="<?php echo $item->link ?>"><?php echo $item->title ?></a>
+                    <p><?php echo $desc ?></p>
+                    <p><?php echo $date ?></p>
+                </li>
+            </ul>
+        <?php
         }
         ?>
 
     </div>
     <div class="sidebar-right">
         <?php
-        $rss = simplexml_load_file("https://rmcsport.bfmtv.com/rss/football/");
+        $url = "https://www.lemonde.fr/economie/rss_full.xml";
+        $rss = simplexml_load_file($url);
 
-        echo '<h4>' . $rss->channel->title . '</h4>';
-
-        foreach ($rss->channel->item as $item) {
-            echo '<h4><a href="' . $item->link . '">' . $item->title . "</a></h4>";
-            echo "<p>" . $item->title . "</p>";
-            echo "<p>" . $item->description . "</p>";
+        for ($i = 0; $i < 12; $i++) {
+            $item = $rss->channel->item[$i];
+            $datetime = date_create($item->pubDate);
+            $date = date_format($datetime, 'd M Y, H\hi');
+            $desc = $item->description;
+            $img = $item->enclosure['url'];
+        ?>
+            <ul>
+                <li>
+                    <img src="<?php echo $img ?>">
+                    <a href="<?php echo $item->link ?>"><?php echo $item->title ?></a>
+                    <p><?php echo $desc ?></p>
+                    <p><?php echo $date ?></p>
+                </li>
+            </ul>
+        <?php
         }
         ?>
+
     </div>
 </div>
 
